043f0d6437a5f74e807179c59e94faef27b3b75c
arnorien <arnaud.rieux@wanadoo.fr>
2022-07-20 15:03:50 +0200

    Ajout d'une installation automatique des dotfiles à partir du dépot github. Ajout de l'installation du plugin zsh-syntax-highlighting

--- newInstall.sh
+++ newInstall.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # Mise à jour et installation des paquets
 sudo apt update
 sudo apt install git vim zsh thefuck curl wget
 sudo apt upgrade && sudo apt dist-upgrade
 
-# Installation de Oh-my-zsh
+# Installation de Oh-my-zsh et zsh-syntax-highlighting plugin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd $ZSH_CUSTOM
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# Récupération des dotfiles
+cd $HOME
+git clone https://github.com/arnorien/dotfiles.git
+rm .zshrc .vimrc
+cp dotfiles/vimrc .vimrc
+cp dotfiles/zshrc .zshrc
